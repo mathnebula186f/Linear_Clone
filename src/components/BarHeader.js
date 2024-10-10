b12d61d5f37f6304e3fc65710254149d44765292
mathnebula186f <getgopalbansal@gmail.com>
2024-10-10 23:01:40 +0530

    Given Assets Used

--- src/components/BarHeader.js
+++ src/components/BarHeader.js
@@ -1,31 +1,39 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircle, faPlus, faEllipsisH, faClipboardCheck, faExclamationTriangle, faClock, faCheckCircle, faTimesCircle, faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
+import TodoIcon from '../UI_ss/Assets/icons_FEtask/To-do.svg'; 
+import Backlog from '../UI_ss/Assets/icons_FEtask/Backlog.svg';
+import InProgress from '../UI_ss/Assets/icons_FEtask/in-progress.svg'
+import Done from '../UI_ss/Assets/icons_FEtask/Done.svg';
+import Cancelled from '../UI_ss/Assets/icons_FEtask/Cancelled.svg';
+import NoPriority from '../UI_ss/Assets/icons_FEtask/3 dot menu.svg';
+import LowPriority from '../UI_ss/Assets/icons_FEtask/Img - Low Priority.svg';
+import HighPriority from '../UI_ss/Assets/icons_FEtask/Img - High Priority.svg';
+import MediumPriority from '../UI_ss/Assets/icons_FEtask/Img - Medium Priority.svg'
+import UrgentPriority from '../UI_ss/Assets/icons_FEtask/SVG - Urgent Priority grey.svg';
 
 // Function to get the appropriate icon based on groupingState and heading
 const getIcon = (groupingState, heading) => {
     if (groupingState === "Status") {
       switch (heading) {
         case "Todo":
-          return faCircle;  // Example icon for "Todo"
+          return TodoIcon;  // Example icon for "Todo"
         case "In progress":
-          return faClock;            // Example icon for "In progress"
+          return InProgress;            // Example icon for "In progress"
         case "Backlog":
-          return faExclamationTriangle; // Example icon for "Backlog"
+          return Backlog; // Example icon for "Backlog"
         case "Cancelled":
-            return faTimesCircle
-        case "Done":
-            return faCheckCircle
+            return Cancelled
         default:
-          return faCircle;          // Default icon for other statuses
+          return Done;          // Default icon for other statuses
       }
     } else if (groupingState === "Priority") {
       const priorityIcons = {
-        0: faEllipsisH, // Icon for priority 0
-        1: faCheckCircle, // Icon for priority 1
-        2: faExclamationTriangle, // Icon for priority 2
-        3: faCircle,      // Icon for priority 3
-        4: faExclamationCircle       // Icon for priority 4
+        0: NoPriority, // Icon for priority 0
+        1: LowPriority, // Icon for priority 1
+        2: MediumPriority, // Icon for priority 2
+        3: HighPriority,      // Icon for priority 3
+        4: UrgentPriority       // Icon for priority 4
       };
       return priorityIcons[heading] || faCircle; // Default icon if not matched
     } else if (groupingState === "User") {
@@ -56,7 +64,7 @@ function BarHeader({groupingState,heading,data,count}) {
     <div style={{marginBottom:"10px"}}>
       <div style={{display:"flex",justifyContent:"space-between",alignItems:"center",padding:"2px"}} >
         <div style={{display:"flex",}}>
-            {icon && <div style={{margin:"0px 4px",color:"grey"}}><FontAwesomeIcon icon={icon} /></div>}
+            {groupingState!=="User" &&  <div style={{margin:"0px 4px",color:"grey"}}><img src={getIcon(groupingState,heading)} alt="Status Icon" /></div>}
             {groupingState==="User" && <div style={{border:"1px solid grey", borderRadius:"50%",margin:"2px",padding:"1px",backgroundColor:"orange",height:"1.5vw",width:"1.5vw",alignItems:"center",display:"flex",justifyContent:"center"}} >{initials}</div>}
             <div style={{margin:"0px 4px"}}>{getHeading(heading)}</div>
             <div style={{margin:"0px 4px",color:"grey"}}>{count} </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEllipsisH,faCircle} from '@fortawesome/free-solid-svg-icons';
+import TodoIcon from '../UI_ss/Assets/icons_FEtask/To-do.svg'; 
+import Backlog from '../UI_ss/Assets/icons_FEtask/Backlog.svg';
+import InProgress from '../UI_ss/Assets/icons_FEtask/in-progress.svg'
+import Done from '../UI_ss/Assets/icons_FEtask/Done.svg';
+import Cancelled from '../UI_ss/Assets/icons_FEtask/Cancelled.svg';
+import NoPriority from '../UI_ss/Assets/icons_FEtask/3 dot menu.svg';
+import LowPriority from '../UI_ss/Assets/icons_FEtask/Img - Low Priority.svg';
+import HighPriority from '../UI_ss/Assets/icons_FEtask/Img - High Priority.svg';
+import MediumPriority from '../UI_ss/Assets/icons_FEtask/Img - Medium Priority.svg'
+import UrgentPriority from '../UI_ss/Assets/icons_FEtask/SVG - Urgent Priority grey.svg';
+
 
 function Card({key,ticket,groupingState,data}) {
 
@@ -11,16 +22,34 @@ function Card({key,ticket,groupingState,data}) {
   // Extract initials from the user's name
   const initials = user ? user.name.split(' ').map(name => name.charAt(0)).join('').toUpperCase() : '??'; // Fallback to '??' if user not found
 
+  function getIcon(status){
+    if(status==="Todo")return TodoIcon;
+    else if(status==="Backlog")return Backlog;
+    else if(status==="In progress")return InProgress;
+    else if(status === "Done")return Done;
+    else return Cancelled;
+  }
+  function getPriorityIcon(priority){
+    if(priority===0)return NoPriority;
+    else if(priority===1)return LowPriority;
+    else if(priority===2)return MediumPriority;
+    else if(priority=== 3)return HighPriority;
+    else return UrgentPriority;
+  }
 //   console.log("Here is a ticket=",ticket) 
   return (
-    <div style={{backgroundColor:"white",width:"fit-content",width:"25vw",marginTop:"10px",borderRadius:"5px",boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",padding:"5px"}}>
+    <div style={{backgroundColor:"white",width:"25vw",marginTop:"10px",borderRadius:"5px",boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",padding:"5px"}}>
       <div style={{display:"flex",justifyContent:"space-between",alignItems:"center",padding:"2px",margin:"2px"}}>
-        <div style={{margin:"2px",color:"grey"}}>{ticket.id}</div>
+        <div style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+          {groupingState!=="Status"&&<div><img src={getIcon(ticket.status)} alt="Status Icon" /></div>}
+          <div style={{margin:"2px",color:"grey"}}>{ticket.id}</div>
+        </div>
+        
         {groupingState!=="User"&&<div style={{border:"1px solid grey", borderRadius:"50%",margin:"2px",padding:"1px",backgroundColor:"orange",height:"1.5vw",width:"1.5vw",alignItems:"center",display:"flex",justifyContent:"center"}} >{initials}</div>}
       </div>
       <div style={{margin:"2px",overflow:"hidden",maxWidth:"200px",fontSize:"small",fontWeight:"bold"}}>{ticket.title}</div>
       <div style={{display:"flex",justifyContent:"flex-start",padding:"2px"}}>
-        <div style={{margin:"0px 3px"}}> <FontAwesomeIcon icon={faEllipsisH} /></div>
+        {groupingState!== "Priority" && <div style={{margin:"0px 3px"}}><img src={getPriorityIcon(ticket.priority)} alt="Priority Icon" /> </div>}
         <div style={{border:"1px solid grey",display:"flex",justifyContent:"space-between",alignContent:"center",margin:"2px",padding:"0px 2px",color:"grey",boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",borderRadius:"5px"}}>
             <div style={{fontSize:"x-small",justifyContent:"center",display:"flex",alignItems:"center",margin:"0px 2px"}}><FontAwesomeIcon icon={faCircle} /></div>
             <div style={{fontSize:"small"}}>Feature Request</div>
